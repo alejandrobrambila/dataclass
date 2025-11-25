b7c4d5c8bcbe2c64c980d3173735583b982a1f77
saskiacampbell <scampbell@thetrustees.org>
2025-11-25 12:46:16 -0500

    ch3

--- exercises_ch2_Saskia.R
+++ exercises_ch2_Saskia.R
@@ -3,18 +3,19 @@
 ## 1. Why does this code not work? ----
 my_variable <- 10
 my_varıable
+    #no dot over i
 
 #Look carefully! (This may seem like an exercise in pointlessness, but training your brain to notice even the tiniest difference will pay off when programming.)
 
 ## 2. Tweak each of the following R commands so that they run correctly. ----
-libary(todyverse)
+library(tidyverse)
 
-ggplot(dTA = mpg) + 
-  geom_point(maping = aes(x = displ y = hwy)) +
-  geom_smooth(method = "lm)
+ggplot(mpg, aes(x = displ, y = hwy)) + 
+  geom_point() +
+  geom_smooth(method = "lm")
   
 ## 3. Press Option + Shift + K / Alt + Shift + K. What happens? How can you get to the same place using the menus? ----
-
+    #gives shortcut menu
 ## 4. Let’s revisit an exercise from the Section 1.6. Run the following lines of code. Which of the two plots is saved as mpg-plot.png? Why?
 
 my_bar_plot <- ggplot(mpg, aes(x = class)) +
@@ -22,3 +23,5 @@ my_bar_plot <- ggplot(mpg, aes(x = class)) +
 my_scatter_plot <- ggplot(mpg, aes(x = cty, y = hwy)) +
   geom_point()
 ggsave(filename = "mpg-plot.png", plot = my_bar_plot)
+
+    #the bar plot was saved because that is what was specified by ggsave
--- exercises_ch3_saskia.R
+++ exercises_ch3_saskia.R
@@ -1,10 +1,6 @@
 # 3.2.5 Exercises
 ##1. In a single pipeline for each condition, find all flights that meet the condition:
-<<<<<<< Updated upstream
-  
-=======
 
->>>>>>> Stashed changes
 ### Had an arrival delay of two or more hours
 ### Flew to Houston (IAH or HOU)
 ### Were operated by United, American, or Delta
@@ -17,15 +13,9 @@
 ## 3. Sort flights to find the fastest flights. (Hint: Try including a math calculation inside of your function.)
 
 ## 4. Was there a flight on every day of 2013?
-<<<<<<< Updated upstream
-  
-### 5. Which flights traveled the farthest distance? Which traveled the least distance?
-  
-=======
 
 ### 5. Which flights traveled the farthest distance? Which traveled the least distance?
 
->>>>>>> Stashed changes
 ### 6. Does it matter what order you used filter() and arrange() if you’re using both? Why/why not? Think about the results and how much work the functions would have to do.
 
 
@@ -35,31 +25,52 @@
 install.packages("nycflights13")
 library(tidyverse)
 library(nycflights13)
+glimpse(nycflights13::flights)
+
+flights|>
+relocate(dep_time, sched_dep_time, dep_delay)
+#dep_time = sched_dep_time +dep_delay
+view(flights)
+
 
 ## 2. Brainstorm as many ways as possible to select dep_time, dep_delay, arr_time, and arr_delay from flights.
+flights |> 
+  select(dep_time, dep_delay, arr_time, arr_delay)
+
+flights |> 
+  select(starts_with("dep"), starts_with("arr"))
 
 
 ## 3. What happens if you specify the name of the same variable multiple times in a select() call?
+flights|>
+  select(dep_time, dep_time)
+  #you still get the variable one time
 
 
 ## 4.  What does the any_of() function do? Why might it be helpful in conjunction with this vector?
-
+any_of()?
+  #have to use with a selecting function
+ 
 variables <- c("year", "month", "day", "dep_delay", "arr_delay")
-
-
+flights |> 
+  select(any_of(variables))
+#selects just from those 5 variables named in vector
 ## 5. Does the result of running the following code surprise you? How do the select helpers deal with upper and lower case by default? How can you change that default?
 
 flights |> select(contains("TIME"))
-
+#contains ignores upper and lower cases
 
 ## 6. Rename air_time to air_time_min to indicate units of measurement and move it to the beginning of the data frame.
-
+flights |>
+rename(air_time_min=air_time)|>
+  relocate(air_time_min)
 
 ## 7. Why doesn’t the following work, and what does the error mean?
 
 flights |> 
   select(tailnum) |> 
   arrange(arr_delay)
+#because the only tailnum was selected, so now R will only work with tailnum
 
 #> Error in `arrange()`:
 #> ℹ In argument: `..1 = arr_delay`.
